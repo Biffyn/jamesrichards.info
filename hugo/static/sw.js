94359d2ed236ea188f3db0d76804ffda9d41ce12
biffyn <nuravenweb@gmail.com>
2019-05-11 15:54:45 +0200

    update: service worker

--- hugo/static/sw.js
+++ hugo/static/sw.js
@@ -4,20 +4,35 @@ if (workbox) {
   workbox.setConfig({ debug: false });
   // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.warn);
 
-  workbox.routing.setDefaultHandler(workbox.strategies.networkFirst({
-    // cacheName: 'fallback',
-  }));
-  
-  workbox.routing.registerRoute(
-    new RegExp(/.*\.(?:js|css)/g),
-    workbox.strategies.networkFirst()
-  );
+  // workbox.routing.setDefaultHandler(workbox.strategies.networkFirst({
+  //   cacheName: 'fallback',
+  // }))
 
-  workbox.routing.registerRoute(
-      new RegExp(/.*\.(?:png|jpg|jpeg|svg|gif|webp)/g),
-      workbox.strategies.cacheFirst()
-  );
+  var defaultStrategy = workbox.strategies.networkFirst({
+    cacheName: 'fallback',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 128,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+        purgeOnQuotaError: true // Opt-in to automatic cleanup
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200] // for opague requests
+      })
+    ]
+  });
+  workbox.routing.setDefaultHandler((args) => {
+    if (args.event.request.method === 'GET') {
+      return defaultStrategy.handle(args); // use default strategy
+    } else {
+      return null;
+    }
+    // return fetch(args.event.request);
+  });
+
+  workbox.routing.registerRoute(new RegExp(/.*\.(?:js|css)/g), workbox.strategies.networkFirst());
+
+  workbox.routing.registerRoute(new RegExp(/.*\.(?:png|jpg|jpeg|svg|gif|webp)/g), workbox.strategies.cacheFirst());
 } else {
   console.log(`No workbox on this browser 😬`);
 }
-
